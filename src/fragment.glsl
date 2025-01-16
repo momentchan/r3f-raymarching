556b23b5ba1f89b730b5d42c646537d0b7f7c092
momentchan <mingjyunhung@gmail.com>
2025-01-16 17:03:36 +0900

    add light and shadow

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -10,25 +10,27 @@ uniform vec2 mousePos;
 uniform float mouseDown;
 
 float GetDist(vec3 p) {
-    vec4 s = vec4(sin(time) * 3.0, 0, 0, 1);
-    float sphere = sdSphere(p - s.xyz, s.w);
 
-    vec3 q = p;  // input copy
-    q.xy = rotate2D(q.xy, time);
+    // Pattern 1
+    // vec4 s = vec4(sin(time) * 3.0, 0, 0, 1);
+    // float sphere = sdSphere(p - s.xyz, s.w);
 
-    q.y -= time * .4;
-    q = fract(q) - .5;
+    // vec3 q = p;  // input copy
+    // q.xy = rotate2D(q.xy, time);
 
-    float box = sdBox(q, vec3(.1));
+    // q.y -= time * .4;
+    // q = fract(q) - .5;
 
-    float ground = p.y + .75;
+    // float box = sdBox(q, vec3(.1));
+
+    // float ground = p.y + .75;
+
+    // return smin(ground, smin(sphere, box, 2.0), 1.0);
 
 
-    return smin(ground, smin(sphere, box, 2.0), 1.0);
-}
 
-float GetDist2(vec3 p) {
-    p.z += time * .4;
+    // Pattern 2
+     p.z += time * .4;
 
     // space repetition
     p.xy = fract(p.xy) - .5;        // spacing: 1
@@ -36,6 +38,17 @@ float GetDist2(vec3 p) {
 
     float box = sdOctahedron(p, .15);
     return box;
+}
+
+vec3 GetNormal(vec3 p) {
+    float d = GetDist(p);
+    vec2 e = vec2(.01, 0);
+    vec3 n = d - vec3(
+        GetDist(p - e.xyy),
+        GetDist(p - e.yxy),
+        GetDist(p - e.yyx));
+
+    return normalize(n);
 }
 
 float RayMarchCustom(vec3 ro, vec3 rd) {
@@ -52,7 +65,7 @@ float RayMarchCustom(vec3 ro, vec3 rd) {
         p.xy = rotate2D(p.xy, dt*.2 * m.x);
         p.y += sin(dt * (m.y + 1.0) * .5) * .35;
 
-        float ds = GetDist2(p);
+        float ds = GetDist(p);
         dt += ds;
 
         if(dt > MAX_DIST || ds < SURF_DIST) break;
@@ -68,7 +81,7 @@ float RayMarch(vec3 ro, vec3 rd) {
     for(int i = 0; i < MAX_STEPS; i++) {
         vec3 p = ro + rd * ds;
 
-        float d = GetDist2(p);
+        float d = GetDist(p);
         ds += d;
 
         if(ds > MAX_DIST || d < SURF_DIST) break;
@@ -76,6 +89,27 @@ float RayMarch(vec3 ro, vec3 rd) {
 
     return ds;
 }
+
+float GetLight(vec3 p) {
+    vec3 lightPos = vec3(.0, 5, 6);
+
+    lightPos.xz += vec2(sin(time), cos(time)) * 2.0;
+
+    vec3 l = normalize(lightPos - p);
+    vec3 n = GetNormal(p);
+    float dif = clamp(dot(n, l), 0., 1.);
+
+
+    // shadow
+
+    vec3 offset = n * SURF_DIST * 2.0;        // offset from the surface because p is very close to surface from 1st raymarch
+    float d = RayMarch(p + offset, l);
+    if(d < length(lightPos - p))        // dist to surface < dist to light
+        dif *= .1;
+        
+    return dif;
+}
+
 
 void mainImage(const in vec4 inputColor, in vec2 uv, out vec4 outputColor)
 {
@@ -93,9 +127,15 @@ void mainImage(const in vec4 inputColor, in vec2 uv, out vec4 outputColor)
     // ro.xz *= rot2D(-mouse.x);
     // rd.xz *= rot2D(-mouse.x);
 
-    float ds = RayMarchCustom(ro, rd);
-    col = palette(ds);
-    // float ds = RayMarch(ro, rd);
+    // float ds = RayMarchCustom(ro, rd);
+    // col = palette(ds);
+    float ds = RayMarch(ro, rd);
+
+    vec3 p = ro + rd * ds;
+
+    float dif = GetLight(p);
+
+    col = vec3(dif);
 
     // Coloring
     outputColor = vec4(col, 1.0);
